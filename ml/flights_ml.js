0b93913e4ac9b2ee27e3a04d4c8bda9261714c52
eloblo <eloblo250@gmail.com>
2022-08-25 16:44:37 +0300

    added ml api

---
+++ ml/flights_ml.js
@@ -0,0 +1,21 @@
+const exp = require('express');
+const {PythonShell} = require('python-shell');
+
+const script = 'ml.py';  // will respond: Normal <= 15, Late > 15, Delayed > 60
+const app = exp();
+
+// API call must contain parameters ['dep_airport', 'arr_airport', 'month', 'day_date', 'day_week', 'hour', 'duration']
+app.get('/flight_delay', function(req,res) {
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], 
+          scriptPath: './ml', 
+        args: [req.query.dep_airport, req.query.arr_airport, req.query.month, req.query.day_date, req.query.day_week, req.query.hour, req.query.duration] 
+    };
+    PythonShell.run(script, options, function (err, results) {
+        if (err) {throw err};
+        res.send(results);
+    })
+})
+
+const server = app.listen(4000);
--- ml/ml.py
+++ ml/ml.py
@@ -4,9 +4,17 @@ import pandas as pd
 
 columns = ['dep_airport', 'arr_airport', 'month', 'day_date', 'day_week', 'hour', 'duration']
 data = [sys.argv[1:]]
-df = pd.DataFrame(data, columns=columns)
-with open('./ml/model.pkl', 'rb') as f:
-    model = pickle.load(f)
-    res = model.predict(df)
-    print(res[0])
-    sys.stdout.flush()
+try:
+    df = pd.DataFrame(data, columns=columns)
+    with open('./ml/model.pkl', 'rb') as f:
+        model = pickle.load(f)
+        res = model.predict(df)
+        if res == 0:
+            print("Normal")
+        elif res == 1:
+            print("Late")
+        else:
+            print("Delayed")
+except Exception as e:
+    print(e)    
+sys.stdout.flush()
