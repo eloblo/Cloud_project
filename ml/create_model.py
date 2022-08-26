f042c332f8ed5042c58ca604f8ffabafb6ad3d34
eloblo <eloblo250@gmail.com>
2022-08-26 14:51:15 +0300

    gradientboost ml model

--- ml/create_model.py
+++ ml/create_model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 import pickle
 
 dataset = './ml/dataset.csv'
@@ -7,7 +7,7 @@ mod_file = './ml/model.pkl'
 df = pd.read_csv(dataset)
 label = df['delay']
 df = df.drop(['delay'], axis=1)
-model = RandomForestClassifier(n_estimators=100)
+model = GradientBoostingClassifier(n_estimators=300, learning_rate=0.1, random_state=42, max_depth=5)
 model.fit(df, label)
 with open(mod_file,'wb') as f:
     pickle.dump(model,f)
