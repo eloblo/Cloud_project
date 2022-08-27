7cd94c4cbdd61e0d7aab26849804298bd61c3244
eloblo <eloblo250@gmail.com>
2022-08-27 14:20:47 +0300

    dataset update

--- ml/api2data.js
+++ ml/api2data.js
@@ -4,13 +4,13 @@ const axios = require('axios');
 const file = './ml/data.txt';
 const params = {
   api_key: 'c453d429-c2f1-42e5-b087-0a701ff0bccf',
-  dep_iata: 'TLV'
+  arr_iata: 'TLV'
 }
 axios.get('https://airlabs.co/api/v9/schedules', {params})  
   .then(response => {
     var apiResponse = response.data.response;
     var d = Buffer.from(JSON.stringify(apiResponse));
-    fs.appendFile(file, d, function(err){
+    fs.writeFile(file, d, function(err){
       if(err) {throw(err)}
     });
   }).catch(error => {
