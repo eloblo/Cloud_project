714dcfd08c29971caafcfb944e2754fcad48d13e
eloblo <eloblo250@gmail.com>
2022-08-30 20:50:35 +0300

    added ml

---
+++ ml/ml.py
@@ -0,0 +1,23 @@
+import sys
+import pickle
+import pandas as pd
+
+# load the model from the .pkl file and predict the delay
+
+columns = ['dep_airport', 'arr_airport', 'month', 'day_date', 'day_week', 'hour', 'duration']
+data = [sys.argv[1:]]
+mod_file = './ml/model.pkl'
+try:
+    df = pd.DataFrame(data, columns=columns)
+    with open(mod_file, 'rb') as f:
+        model = pickle.load(f)
+        res = model.predict(df)
+        if res == 0:
+            print("Normal")
+        elif res == 1:
+            print("Late")
+        else:
+            print("Delayed")
+except Exception as e:
+    print(e)    
+sys.stdout.flush()
---
+++ ml/system-c.js
@@ -0,0 +1,28 @@
+const exp = require('express');
+const {PythonShell} = require('python-shell');
+const consumer = require('../consumer_mongo');
+
+const script = 'ml.py';  // will respond: Normal <= 15, Late > 15, Delayed > 60
+const app = exp();
+
+// API call must contain parameters ['dep_airport', 'arr_airport', 'month', 'day_date', 'day_week', 'hour', 'duration']
+app.get('/flight_delay', function(req,res) {
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], 
+          scriptPath: './ml', 
+        args: [req.query.dep_airport, req.query.arr_airport, req.query.month, req.query.day_date, req.query.day_week, req.query.hour, req.query.duration] 
+    };
+    // run the ML script and send the result
+    PythonShell.run(script, options, function (err, results) {
+        if (err) {throw err};
+        res.send(results);
+    })
+})
+
+const port = 4000
+const server = app.listen(port);
+
+console.log(`system-c: flight_delay machine learning server on port ${port}`)
+// start consuming landed flight data and upload to mongodb
+consumer.save_flights()
