f34016fcd92f6cdf2a6dc4c6886ed85bcf004e83
eloblo <eloblo250@gmail.com>
2022-08-30 11:59:42 +0300

    new ML model

--- ml/create_model.py
+++ ml/create_model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 # create a ML model and save it as a file to reduce time
@@ -9,7 +9,7 @@ mod_file = './ml/model.pkl'
 df = pd.read_csv(dataset)
 label = df['delay']
 df = df.drop(['delay'], axis=1)
-model = GradientBoostingClassifier(n_estimators=300, learning_rate=0.1, random_state=42, max_depth=5)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(df, label)
 with open(mod_file,'wb') as f:
     pickle.dump(model,f)
