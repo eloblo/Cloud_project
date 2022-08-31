6614b6e54e9c0119ecbfb1b24533a6d53c9668f7
eloblo <eloblo250@gmail.com>
2022-08-31 22:29:40 +0300

    dataset update

--- ml/create_model.py
+++ ml/create_model.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 # create a ML model and save it as a file to reduce time
-
+#dataset = './ml/'dataset_flights.csv
 dataset = './ml/dataset.csv'
 mod_file = './ml/model.pkl'
 df = pd.read_csv(dataset)
