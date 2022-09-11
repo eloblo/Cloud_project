ceb658d882eddb809b22ae9f775560e534de069c
Uriya Shalom <uriyas10@gmail.com>
2022-09-11 11:59:49 +0300

    fix: style

--- public/js/2land.js
+++ public/js/2land.js
@@ -2,6 +2,5 @@ const socket_land = io();
 
 socket_land.on("2land", function(data){
     var card = document.getElementById("2land");
-    card.innerHTML=`<h1><b>Flights before landing</b></h1>
-    <h2><b>${data}<b></h2>`;
+    card.innerHTML=`<br><h3><b>Current tempeture at TLV</b></h3><h4><b>${data}<b></h4>`;
 })
