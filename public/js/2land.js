f6ccb8cfec0d06b8c4a693191dc74beab60b57c7
eloblo <eloblo250@gmail.com>
2022-09-11 12:10:03 +0300

    2land fix

--- public/js/2land.js
+++ public/js/2land.js
@@ -2,5 +2,5 @@ const socket_land = io();
 
 socket_land.on("2land", function(data){
     var card = document.getElementById("2land");
-    card.innerHTML=`<br><h3><b>Current tempeture at TLV</b></h3><h4><b>${data}<b></h4>`;
+    card.innerHTML=`<br><h3><b>Flights before landing</b></h3><h4><b>${data}<b></h4>`;
 })
